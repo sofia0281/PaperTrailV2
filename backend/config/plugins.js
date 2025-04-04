6e8c403ec3e2af3f0221fb0e92aa726721545f9d
Sebastian Perdomo Rengifo <Sapr262004@gmail.com>
2025-04-04 10:26:13 -0500

    update

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = ({}) => ({});
+module.exports = ({ env }) => ({
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('SMTP_HOST', 'smtp.example.com'), // Servidor SMTP
+          port: env.int('SMTP_PORT', 587), // Puerto SMTP
+          auth: {
+            user: env('SMTP_USERNAME'), // Tu usuario SMTP
+            pass: env('SMTP_PASSWORD'), // Tu contraseña SMTP
+          },
+          // ... otras configuraciones de Nodemailer
+        },
+        settings: {
+          defaultFrom: env('SMTP_DEFAULT_FROM', 'no-reply@example.com'),
+          defaultReplyTo: env('SMTP_DEFAULT_REPLY_TO', 'no-reply@example.com'),
+        },
+      },
+    },
+  });
